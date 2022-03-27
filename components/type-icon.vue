f53fea46f65abe6ee134af3bebf3bfa1c4351936
unknown <1339531047@qq.com>
2022-03-28 02:25:14 +0800

    feat: 新增类型图标组件

---
+++ components/type-icon.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="container" :style="{ background: color }">
+		<image :src="'/static/type/' + type + '.png'"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"type-icon",
+		props: {
+			type: String,
+			color: {
+				type: String,
+				value: '#69c0ff'
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		width: 72rpx;
+		height: 72rpx;
+		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		image {
+			width: 40rpx;
+			height: 40rpx;
+		}
+	}
+</style>
