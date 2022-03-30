73bdcb518ad9256ea20de9470f2e0d9fe66da2c2
unknown <1339531047@qq.com>
2022-03-31 02:35:38 +0800

    feat: 新增修改收支和删除收支方法

--- uniCloud-aliyun/cloudfunctions/income-expend/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/income-expend/index.obj.js
@@ -1,4 +1,6 @@
 const uniID = require('uni-id')
+const db = uniCloud.database()
+const collection = db.collection('income-expend')
 
 module.exports = {
 	/**
@@ -20,9 +22,6 @@ module.exports = {
 				errMsg: '用户信息错误'
 			}
 		}
-
-		const db = uniCloud.database()
-		const collection = db.collection('income-expend')
 
 		try {
 			await collection.add({
@@ -47,6 +46,55 @@ module.exports = {
 			return {
 				errCode: 11000,
 				errMsg: '入账失败，请稍后重试'
+			}
+		}
+	},
+	async update (id, type, time, money, name, py, subname, account, remark) {
+		try {
+			const { updated } = await collection.where({
+				_id: id
+			}).update({
+				type,
+				time: Date.parse(time),
+				money: money * 100,
+				name,
+				py,
+				subname,
+				account,
+				remark
+			})
+			
+			if (updated > 0) {
+				return {
+					errCode: 0,
+					errMsg: '修改成功'
+				}
+			} else {
+				return {
+					errCode: 11001,
+					errMsg: '数据没有变化呀'
+				}
+			}
+		} catch (e) {
+			return {
+				errCode: 11002,
+				errMsg: '数据修改失败，请稍后重试'
+			}
+		}
+	},
+	async delete (id) {
+		try {
+			const { deleted } = await collection.doc(id).remove()
+			if (deleted > 0) {
+				return {
+					errCode: 0,
+					errMsg: '删除成功'
+				}
+			}
+		} catch (e) {
+			return {
+				errCode: 11003,
+				errMsg: '删除失败，请稍后重试'
 			}
 		}
 	}
